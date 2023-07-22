32eeb8ede08eea0aef6ad9b83ec2930978f7d970
rt7 <oyasal@gmail.com>
2023-07-22 17:14:02 +0300

    install.sh update

--- install.sh
+++ install.sh
@@ -21,15 +21,15 @@ git checkout docking
 mkdir -p build
 echo "${CIMGUI} build folder created"
 
-LIB_IMGUI="-I./lib/imgui/ -I./lib/imgui/backends -I./lib/imgui/docs -I./lib/imgui/examples -I./lib/imgui/mics"
+LIB_IMGUI="-I./ -I./backends -I./docs -I./examples -I./mics"
 
-g++ ${LIB_IMGUI} -o ./build/imgui_demo.o -c imgui_demo.cpp
-g++ ${LIB_IMGUI} -o ./build/imgui_draw.o -c imgui_draw.cpp
-g++ ${LIB_IMGUI} -o ./build/imgui_tables.o -c imgui_tables.cpp
-g++ ${LIB_IMGUI} -o ./build/imgui_widgets.o -c imgui_widgets.cpp
-g++ ${LIB_IMGUI} -o ./build/imgui.o -c imgui.cpp
+g++  -o ./build/imgui_demo.o -c imgui_demo.cpp ${LIB_IMGUI}
+g++  -o ./build/imgui_draw.o -c imgui_draw.cpp ${LIB_IMGUI}
+g++  -o ./build/imgui_tables.o -c imgui_tables.cpp ${LIB_IMGUI}
+g++  -o ./build/imgui_widgets.o -c imgui_widgets.cpp ${LIB_IMGUI}
+g++  -o ./build/imgui.o -c imgui.cpp ${LIB_IMGUI}
 
-g++ ${LIB_IMGUI} -o ./build/imgui_impl_opengl3.o -c ./backends/imgui_impl_opengl3.cpp
-g++ ${LIB_IMGUI} -o ./build/imgui_impl_glfw.o -c ./backends/imgui_impl_glfw.cpp
+g++  -o ./build/imgui_impl_opengl3.o -c ./backends/imgui_impl_opengl3.cpp ${LIB_IMGUI}
+g++  -o ./build/imgui_impl_glfw.o -c ./backends/imgui_impl_glfw.cpp ${LIB_IMGUI}
 
 echo "${CIMGUI} compilation is done!"
